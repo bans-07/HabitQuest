b86338117e966eb60e36034f7418cbad6a9676ad
Your GitHub Username <your-email@example.com>
2025-05-23 13:50:52 -0500

    added package.json,fixed typescript errors

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import App from "./App";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
