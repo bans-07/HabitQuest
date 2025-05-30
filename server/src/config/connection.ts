4d5e1c94216680895ead9d96fd850ef977e8cc9f
Jen <jensavino26@gmail.com>
2025-05-29 23:36:57 -0400

    Changes to resolvers

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -6,9 +6,6 @@ dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/habitquest_db';
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(MONGODB_URI)
 
 export default mongoose.connection;
