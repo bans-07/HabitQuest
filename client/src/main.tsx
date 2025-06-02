0bd4cac764cb58c071ba0edf2b2f4a986a460b34
Jen <jensavino26@gmail.com>
2025-06-02 13:40:39 -0400

    edits to maintsx file to try to resolve 404 button erros

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import App from './App.jsx';;
+import App from './App.jsx';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard.js';
@@ -11,17 +11,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-    {
-        index: true,
-        element: <Login />
-      }, {
-        path: '/signup',
-        element: <Signup />
-      },
-      {
-        path: '/dashboard',
-        element: <Dashboard />
-      },
+      { index: true, element: <Login /> },
+      { path: 'login', element: <Login /> },
+      { path: 'signup', element: <Signup /> },
+      { path: 'dashboard', element: <Dashboard /> },
     ]
   },
 ]);
